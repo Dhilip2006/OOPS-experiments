4ec3ae5248670b60b6c8265596d0a2fd627e9617
Dhilip-2024-aia <rdhilip2006@gmail.com>
2025-08-28 17:56:54 +0530

    Updated ExceptionHandlingDemo.java for division logical mistake

--- ExceptionHandlingDemo.java
+++ ExceptionHandlingDemo.java
@@ -1,6 +1,5 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class ExceptionHandlingDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,22 +14,29 @@ public class ExceptionHandlingDemo {
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
             }
-
             System.out.print("Enter first index: ");
             int i1 = sc.nextInt();
             System.out.print("Enter second index: ");
             int i2 = sc.nextInt();
             int a = arr[i1];
             int b = arr[i2];
-            System.out.println("Choose operation: 1-Addition  2-Subtraction  3-Multiplication  4-Division");
+            System.out.println("Element at index " + i1 + " = " + a);
+            System.out.println("Element at index " + i2 + " = " + b);
+            System.out.println("Choose operation: ");
+            System.out.println("1-Addition  2-Subtraction  3-Multiplication  4-Division");
             int choice = sc.nextInt();
-
             int result = 0;
             switch (choice) {
                 case 1: result = a + b; break;
                 case 2: result = a - b; break;
                 case 3: result = a * b; break;
-                case 4: result = a / b; break; 
+                case 4:
+                    if (b == 0) {
+                        throw new ArithmeticException("Division by zero!");
+                    }
+                    double divResult = (double) a / b;
+                    System.out.println("Result = " + divResult);
+                    return;
                 default: 
                     System.out.println("Invalid choice!");
                     return;
